ffe32f33304c7166eaf22687ee39e892cd606c3c
Kushagra Singh <44263206+sportykush@users.noreply.github.com>
2021-06-15 10:25:19 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 function connect() {
     mongoose
-        .connect('mongodb+srv://sportykush:9826956831@cluster0.bnnbv.mongodb.net/memezar?retryWrites=true&w=majority', { useUnifiedTopology: true })
+        .connect('mongodb+srv://sportykush:<password>@cluster0.bnnbv.mongodb.net/memezar?retryWrites=true&w=majority', { useUnifiedTopology: true })
         .then( () => {
             console.log("Connected to the database!");
         })
